bbf95e18ac0b7d4041a4fbed93f9c0aba4292b4c
adela123450 <adelasilvag95@gmail.com>
2026-04-11 12:13:05 -0500

    V3 Agrego el ProductGrid con nuevos productos y productos destacados

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,18 @@
 import { FeatureGrid } from "../components/home/FeatureGrid";
+import { ProductGrid } from "../components/home/ProducGrid";
 
 export const HomePage = () => {
   return (
     <div>
-      <h1>Bienvenido a nuestra tienda</h1>
       <FeatureGrid />
+
+      <ProductGrid 
+          title="Nuevos Productos" 
+          products={[{id: 1, title: "Producto 1"}]} />
+
+      <ProductGrid 
+          title="Productos Destacados" 
+          products={[{id: 2, title: "Producto Destacado 1"}]} />
     </div>
   );
 };
